85577f7d8fbcdc7de7e5a68a789fe7ba97bcd578
Alvaro Miranda <al512varo@gmail.com>
2023-05-05 11:41:43 -0300

    Sexto Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menu = [
+    {
+        id: 1,
+        tittle: 'Buttermilk Pancakes',
+        category: 'Breakfast',
+        image: './images/item-1.jpeg'
+        description: "I'm baby woke milshake wolf bitters live-edge blue bottle, hamrock freegan copper whatever cold-pressed",
+    },
+
+    {
+        id: 2,
+        tittle: 'Diner Double',
+        category: 'Lunch',
+        image: './images/item-2.jpeg'
+        description: "I'm baby woke milshake wolf bitters live-edge blue bottle, hamrock freegan copper whatever cold-pressed",
+    },
+]
