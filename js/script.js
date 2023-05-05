db3e20880e81ea98d0e0444130d00b6df4a5cab2
Alvaro Miranda <al512varo@gmail.com>
2023-05-05 12:27:48 -0300

    Setimo commit

--- js/script.js
+++ js/script.js
@@ -3,15 +3,89 @@ const menu = [
         id: 1,
         tittle: 'Buttermilk Pancakes',
         category: 'Breakfast',
-        image: './images/item-1.jpeg'
-        description: "I'm baby woke milshake wolf bitters live-edge blue bottle, hamrock freegan copper whatever cold-pressed",
+        price: 15.99,
+        image: './images/item-1.jpeg',
+        description: "I'm baby woke milshake wolf bitters live-edge blue bottle, hamrock freegan copper whatever cold-pressed.",
     },
 
     {
         id: 2,
         tittle: 'Diner Double',
         category: 'Lunch',
-        image: './images/item-2.jpeg'
-        description: "I'm baby woke milshake wolf bitters live-edge blue bottle, hamrock freegan copper whatever cold-pressed",
+        price: 12.99,
+        image: './images/item-2.jpeg',
+        description: "Vaporware iphone mumnlecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing.",
     },
-]
+
+    {
+        id: 3,
+        tittle: 'Godzilla Milkshake',
+        category: 'Shakes',
+        price: 6.99,
+        image: './images/item-3.jpeg',
+        description: "Ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+    },
+
+    {
+        id: 4,
+        tittle: 'Country Delight',
+        category: 'Brea',
+        price: 20.99,
+        image: './images/item-4.jpeg',
+        description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch.Prism austin mlkshk truffaut.",
+    },
+
+    {
+        id: 5,
+        tittle: 'Egg Attack',
+        category: 'Lunch',
+        price: 22.99,
+        image: './images/item-5.jpeg',
+        description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up.",
+    },
+
+    {
+        id: 6,
+        tittle: 'Oreo Dream',
+        category: 'Shakes',
+        price: 18.99,
+        image: './images/item-6.jpeg',
+        description: "Portland chicharrones ethical edison, palo santo craft beer chia heirloom iphone everyday.",
+    },
+
+    {
+        id: 7,
+        tittle: 'Bacon Overflow',
+        category: 'BreakFast',
+        price: 8.99,
+        image: './images/item-7.jpeg',
+        description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird.",
+    },
+
+    {
+        id: 8,
+        tittle: 'American Classic',
+        category: 'Lunch',
+        price: 12.99,
+        image: './images/item-8.jpeg',
+        description: "On it tumblr kickstarter thundercast migas everyday carry squid palo santo leggings. Food truck truffaut.",
+    },
+
+    {
+        id: 9,
+        tittle: 'Quarantine Buddy',
+        category: 'Shakes',
+        price: 16.99,
+        image: './images/item-9.jpeg',
+        description: "Skateboard fam synth authentic semiotics. Live-edge lyft af, edison buld yuccie crucifix microdosing.",
+    },
+
+    {
+        id: 10,
+        tittle: 'Stack Dinner',
+        category: 'Dinner',
+        price: 39.99,
+        image: './images/item-10.jpeg',
+        description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up.",
+    },
+];
